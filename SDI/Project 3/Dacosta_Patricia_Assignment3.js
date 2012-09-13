ec2ba36d683ca82940c38fde8329e5d0b033bd32
Patti Dacosta <pattidacosta@cfl.rr.com>
2012-09-13 15:07:46 -0400

    Added number function, while loop and a currently commented out nested for loop, as it is not working properly.

--- SDI/Project 3/Dacosta_Patricia_Assignment3.js
+++ SDI/Project 3/Dacosta_Patricia_Assignment3.js
@@ -8,95 +8,100 @@
 
 var myEvent 	= "Halloween Horror Nights";
 var pplGoing 	= 4;
+var myCity		= "Orlando"
 var weCanFit 	= true;
-var toDrive 	= [
-				"Durango",
-				"Miata",
-				"Aveo"
+var whoGoes 	= [
+				"George",
+				"Kari",
+				"JJ",
+				"I"
 ];
 
-//Object
-
+/*
 var needFood = {
-        "peopleMenu": [
+        "peopleEat": [
             {	
-            	"relation":	"hubby",
                 "name": 	"George",
                 "food": 	"pizza",
-                "drink": 	"fruit punch Gatorade" 
+                "drink": 	"fruit punch Gatorade",
+                "Cost":		7
             },
             {
-            	"relation":	"myself",
-                "name": 	"I",
+                "name": 	"Patti",
                 "food": 	"fruit salad",
-                "drink": 	"orange Gatorade" 
+                "drink": 	"orange Gatorade",
+                "cost":		8 
             },
             {
-            	"relation":	"daughter",
                 "name": 	"Kari",
                 "food": 	"fetuccini alfredo",
-                "drink": 	"orange Gatorade" 
+                "drink": 	"orange Gatorade",
+                "cost":		9
             },
             {
-            	"relation":	"son",
                 "name": 	"JJ",
                 "food": 	"pizza",
-                "drink": 	"Pepsi"
+                "drink": 	"Pepsi",
+                "cost":		7
             }            
         ]
         
 };
-
+*/
 	
 
 
 //Method Procedure
 
 var whatToDo = {
-    haveFun: "go out",
-    whereTo: "Halloween Horror Nights",
-    doWhat: ["Haunted Houses", "Scarezones", "Shows"],
-    booProc: true,    
-    timeToGo: function (getAway) {
+    		haveFun: "go out",
+    		whereTo: "Halloween Horror Nights",
+    		doWhat: ["Haunted Houses", "Scarezones", "Shows"],
+    		booProc: true,    
+    		timeToGo: function (getAway) {
     var haveFun = getAway;
         if (whatToDo.booProc === true) {
 	    console.log("We want to " + whatToDo.haveFun + "! So, we will go to " + whatToDo.whereTo + ", and get to see " + whatToDo.doWhat[0] + ", " + whatToDo.doWhat[1] + " and " + whatToDo.doWhat[2] + "!");
 	} else {
-	    console.log("Sadly, we are unable to " + whatToDo.haveFun + ", maybe another time.") 	};   
+	    console.log("Sadly, we are unable to " + whatToDo.haveFun + ", maybe another time.") };   
 	    }
-};
+};	    
+	    
+//Number Function
 
-//Boolean Function
-/*
+var numbReturn = function (milesToGo) {
+	var milesFromHome = 0;
+	var milesToGo = 80;
+	while (milesToGo > 0) {
+			console.log("We only have " + milesToGo + " miles, to go!");
+	milesToGo-=10;
+	
+		/*for (var milesFromHome = 0; milesFromHome < 80; milesFromHome+=20)
+		console.log("We have to stop after " + milesFromHome + " miles, so that " + whoGoes[i] + " can go to the bathroom!");*/
+	} 
+	return milesToGo;	
+};			
 
-var booFunc = function (blah1, blah2) {
-	var booReturn;
-	if ((blah1 || blah2) && !(blah1 && blah2)) {
-			booReturn = true;
-			console.log();
-	} else {
-			booReturn = false;
-			console.log();
-	};
-	return booReturn;
-};
+
+//Mutator Method
+
 
 //Number Function
 
-*/
 //Method Accessor
-//Add another loop & concat for wait or remove wait in houses objects...
+//Possible for 2nd loop & concat for wait or remove wait in houses objects?
 var handleData = function (data) {	
 	for (var i = 0; i < data.houses.length; i++){
 		var house = data.houses[i];
 		console.log(house.name + " is a " + house.theme + " themed house. We saw " + house.scareactors + ". " + "It took about " + house.howLong + " minutes, to go through the house.");
 		
-	}; 
+	};
+	return data; 
 };
 whatToDo.timeToGo();
+console.log("Time to drive to " + myCity + "!");
+numbReturn();
 handleData(houseData);
-
-
 
 
 
