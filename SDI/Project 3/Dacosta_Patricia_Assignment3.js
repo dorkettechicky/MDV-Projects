9d74f3add1d4b02dc98b6af8f939fe83f496cb02
Patti Dacosta <pattidacosta@cfl.rr.com>
2012-09-13 03:15:16 -0400

    Added additional var's, additional objects and Method Procedure.

--- SDI/Project 3/Dacosta_Patricia_Assignment3.js
+++ SDI/Project 3/Dacosta_Patricia_Assignment3.js
@@ -6,12 +6,64 @@
 
 //Global Variables
 
-var myEvent = "Halloween Horror Nights";
+var myEvent 	= "Halloween Horror Nights";
+var pplGoing 	= 4;
+var booVar 		= true;
+var toDo 		= [
+				"scarezones",
+				"haunted houses",
+				"shows",
+				"grab a bite"
+];
 
-var handleData = function (data) {
+//Object
 
+var needFood = {
+        "peopleMenu": [
+            {	
+            	"relation":	"hubby",
+                "name": 	"George",
+                "food": 	"pizza",
+                "drink": 	"fruit punch Gatorade" 
+            },
+            {
+            	"relation":	"myself",
+                "name": 	"I",
+                "food": 	"fruit salad",
+                "drink": 	"orange Gatorade" 
+            },
+            {
+            	"relation":	"daughter",
+                "name": 	"Kari",
+                "food": 	"fetuccini alfredo",
+                "drink": 	"orange Gatorade" 
+            },
+            {
+            	"relation":	"son",
+                "name": 	"JJ",
+                "food": 	"pizza",
+                "drink": 	"Pepsi"
+            }            
+        ]
+        
+};
+
+	
+
+
+//Method Procedure
+
+whatToDo: function(whereToGo){
+	if (myEvent == "Halloween Horror Nights") {
+		console.log("We want to go to " + myEvent + "and get chased by zombies!");
+		} else	{
+			console.log("We haz a sad.");	
+	};
+	
+};
 
 //Boolean Function
+/*
 
 var booFunc = function (blah1, blah2) {
 	var booReturn;
@@ -27,11 +79,11 @@ var booFunc = function (blah1, blah2) {
 
 //Number Function
 
-
-
-
-	
-for (var i = 0; i < data.houses.length; i++){
+*/
+//Method Accessor
+//Add another loop & concat for wait or remove wait in houses objects...
+var handleData = function (data) {	
+	for (var i = 0; i < data.houses.length; i++){
 		var house = data.houses[i];
 		console.log(house.name + " is a " + house.theme + " themed house. We saw " + house.scareactors + ". " + "It took about " + house.howLong + " minutes, to go through the house.");
 		
