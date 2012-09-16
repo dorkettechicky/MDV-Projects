f1d8f84ea4e00f36107265c8a2166f3feeb02235
Patti Dacosta <pattidacosta@cfl.rr.com>
2012-09-15 20:28:04 -0400

    Initial file setup

---
+++ SDI/Project 4/Dacosta_Patricia_Assignment4.js
@@ -0,0 +1,28 @@
+//Patricia Dacosta
+//SDI 1209
+//Project 4
+//String, Number and Array manipulation & regular expressions.
+
+//Phone Number Pattern
+
+//Email Address Pattern
+
+//URL Pattern
+
+//Title-case
+
+//String separator manipulation
+
+//Decimal places
+
+//Fuzzy match
+
+//Difference between two dates (hours or days)
+
+//Variable to number
+
+//Smallest vaue in an array that is greater than a given number
+
+//Total value of just the numbers in an array
+
+//Array of objects sorted by key
