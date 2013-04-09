136514270bb4544796f82f2eb5f28936f63ab606
Patti Dacosta <pattidacosta@cfl.rr.com>
2013-04-09 12:50:31 -0400

    Initial commit project 1. Project created & located didFinishLaunchingWithOptions

---
+++ AOC1/Project1/Project1Tests/Project1Tests.h
@@ -0,0 +1,13 @@
+//
+//  Project1Tests.h
+//  Project1Tests
+//
+//  Created by Patti Dacosta on 4/9/13.
+//  Copyright (c) 2013 Patti Dacosta. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Project1Tests : SenTestCase
+
+@end
---
+++ AOC1/Project1/Project1Tests/Project1Tests.m
@@ -0,0 +1,32 @@
+//
+//  Project1Tests.m
+//  Project1Tests
+//
+//  Created by Patti Dacosta on 4/9/13.
+//  Copyright (c) 2013 Patti Dacosta. All rights reserved.
+//
+
+#import "Project1Tests.h"
+
+@implementation Project1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Project1Tests");
+}
+
+@end
