c0e0b8043cf3c6a7f0a7255cd664393ec9986214
Patti Dacosta <pattidacosta@cfl.rr.com>
2013-04-15 13:31:37 -0400

    Labels added with exception of list of items array.

--- AOC1/Project2/Project2/ViewController.m
+++ AOC1/Project2/Project2/ViewController.m
@@ -16,6 +16,107 @@
 
 - (void)viewDidLoad
 {
+    //Begin Assignment Code
+    
+    //Change background color
+    self.view.backgroundColor = [UIColor colorWithRed:0.22 green:0.22 blue:0.22 alpha:1]; /*#383838*/
+    
+    //Book Title with if check, background in 1 color text in another centered.
+    bookTitle = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 0.0f, 310.0f, 40.0f)];
+    
+    if (bookTitle !=nil){
+        bookTitle.backgroundColor = [UIColor colorWithRed:0.298 green:0.169 blue:0.184 alpha:1]; /*#4c2b2f*/
+        bookTitle.text = @"Split Infinity:";
+        bookTitle.textColor = [UIColor colorWithRed:1 green:0.937 blue:0.765 alpha:1]; /*#ffefc3*/
+        bookTitle.textAlignment = NSTextAlignmentCenter;
+    }
+    [self.view addSubview:bookTitle];
+    
+    //Author Label with if check, background & text in different colors, text align right.
+    authorLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 50.0f, 120.0f, 40.0f)];
+    
+    if (authorLabel !=nil){
+        authorLabel.backgroundColor = [UIColor colorWithRed:0.898 green:0.443 blue:0.322 alpha:1]; /*#e57152*/
+        authorLabel.text = @"Author:";
+        authorLabel.textColor = [UIColor colorWithRed:0.91 green:0.871 blue:0.404 alpha:1]; /*#e8de67*/
+        authorLabel.textAlignment = NSTextAlignmentRight;
+    }
+    [self.view addSubview:authorLabel];
+    
+    //Author Name with if check, background & text in different colors, text align left.
+    authorName = [[UILabel alloc] initWithFrame:CGRectMake(125.0f, 50.0f, 190.0f, 40.0f)];
+    
+    if (authorName !=nil){
+        authorName.backgroundColor = [UIColor colorWithRed:0.302 green:0.545 blue:0.302 alpha:1]; /*#4d8b4d*/
+        authorName.text = @"Piers Anthony";
+        authorName.textColor = [UIColor colorWithRed:0.294 green:0.224 blue:0.161 alpha:1]; /*#4b3929*/
+        authorName.textAlignment = NSTextAlignmentLeft;
+    }
+    [self.view addSubview:authorName];
+    
+    //Published Label with if check, background & text in different colors, text align right.
+    publishedLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 100.0f, 120.0f, 40.0f)];
+    
+    if (publishedLabel !=nil){
+        publishedLabel.backgroundColor = [UIColor colorWithRed:0.298 green:0.439 blue:0.416 alpha:1]; /*#4c706a*/
+        publishedLabel.text = @"Published:";
+        publishedLabel.textColor = [UIColor colorWithRed:0.529 green:0.196 blue:0.173 alpha:1]; /*#87322c*/
+        publishedLabel.textAlignment = NSTextAlignmentRight;
+    }
+    [self.view addSubview:publishedLabel];
+    
+    //Published Date with if check, background & text in different colors, text align left.
+    publishedDate = [[UILabel alloc] initWithFrame:CGRectMake(125.0f, 100.0f, 190.0f, 40.0f)];
+    
+    if (publishedDate !=nil){
+        publishedDate.backgroundColor = [UIColor colorWithRed:0.49 green:0.612 blue:0.514 alpha:1]; /*#7d9c83*/
+        publishedDate.text = @"April 1980";
+        publishedDate.textColor = [UIColor colorWithRed:0.749 green:0.51 blue:0.42 alpha:1]; /*#bf826b*/
+        publishedDate.textAlignment = NSTextAlignmentLeft;
+    }
+    [self.view addSubview:publishedDate];
+    
+    //Summary Label with if check, background & text in different colors, text align left.
+    summaryLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 150.0f, 115.0f, 40.0f)];
+    
+    if (summaryLabel !=nil){
+        summaryLabel.backgroundColor = [UIColor colorWithRed:0.039 green:0.294 blue:0.49 alpha:1]; /*#0a4b7d*/
+        summaryLabel.text = @"Summary:";
+        summaryLabel.textColor = [UIColor colorWithRed:0.678 green:0.169 blue:0.596 alpha:1]; /*#ad2b98*/
+        summaryLabel.textAlignment = NSTextAlignmentLeft;
+    }
+    [self.view addSubview:summaryLabel];
+    
+    //Summary Text with if check, background & text in different colors, text align center.
+    summaryText = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 200.0f, 310.0f, 270.0f)];
+    
+    if (summaryText !=nil){
+        summaryText.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1]; /*#000000*/
+        summaryText.text = @"Stile serves the citizens of Proton as a serf. While working as a jockey, there are several attempts on his life. He begins to run for his life. Suddenly he sees a shimmering curtain, which transports him in to a parallel world where magic operates, not science. He discovers that his alternate self was murdered in the parallel world known as Phaze. Stile must now master magic to take the Blue Adept's place.";
+        summaryText.textColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:1]; /*#ffffff*/
+        summaryText.textAlignment = NSTextAlignmentCenter;
+        summaryText.numberOfLines = 11;
+    }
+    [self.view addSubview:summaryText];
+    
+    //List of Items Label with if check, background & text in different colors, text align left.
+    listLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 480.0f, 115.0f, 40.0f)];
+    
+    if (listLabel !=nil){
+        listLabel.backgroundColor = [UIColor colorWithRed:0.451 green:0.114 blue:0.631 alpha:1]; /*#731da1*/
+        listLabel.text = @"List of Items:";
+        listLabel.textColor = [UIColor colorWithRed:1 green:0.569 blue:0.459 alpha:1]; /*#ff9175*/
+        listLabel.textAlignment = NSTextAlignmentLeft;
+    }
+    [self.view addSubview:listLabel];
+
+
+
+
+
+
+
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
